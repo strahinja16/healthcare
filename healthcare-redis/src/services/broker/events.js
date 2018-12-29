29a897d5f341a293c2be744cbb75552b10431e0a
JKostov <julijekos@gmail.com>
2018-12-29 01:40:58 +0100

    Event handler for prescribed drug done

--- healthcare-redis/src/services/broker/events.js
+++ healthcare-redis/src/services/broker/events.js
@@ -1,9 +1,18 @@
 const logger = require('services/logger');
+const axios = require('api');
+const drugService = require('services/redis/drug');
 
 module.exports = {
-  'redis.event': async (data, sender) => {
+  'drug.prescribed': async (data, sender) => {
     try {
+        const { drugName } = data;
+        const { data: sideEffects } = await axios.get(`/side-effects/${drugName}`);
 
+        const se = await drugService.getSideEffectsForDrug(drugName);
+
+        if(se.length === 0) {
+            await drugService.setSideEffectsForDrug(drugName, sideEffects);
+        }
     } catch (ex) {
       logger.error(ex);
     }
